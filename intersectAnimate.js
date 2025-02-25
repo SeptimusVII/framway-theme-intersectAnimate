dedfa01032d8f2897a2f21ec5baa292f35fa5466
Quentin Vansteene <vansteene.quentin@gmail.com>
2025-02-25 15:55:17 +0100

    add base styles and js

--- intersectAnimate.js
+++ intersectAnimate.js
@@ -1,3 +1,13 @@
-$(function(){
-
+const observer = new IntersectionObserver((entries)=>{
+	entries.forEach((entry)=>{
+		// console.log(entry)
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show')
+		} else {
+			entry.target.classList.remove('show')
+		}
+	})
 });
+
+const animateElements = document.querySelectorAll('[class*=animate--]');
+animateElements.forEach((el)=> observer.observe(el));
